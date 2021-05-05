554c81a960dfaaf7468e229570e1d5efefb24785
Pablo Klaschka <contact@pabloklaschka.de>
2021-05-05 14:18:35 +0200

    feat(message-transformer): Add initial implementation for the `MessageTransformer` verticle
    While not final yet (since I'm waiting for the correct MavLink package definitions), this is the basic structure of the verticle which I'll adjust later.
    
    Co-authored-by: cb0s <cedric.boes@online.de>

---
+++ application/src/main/java/de/wuespace/telestion/project/daedalus2/TransformedMessage.java
@@ -0,0 +1,10 @@
+package de.wuespace.telestion.project.daedalus2;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import de.wuespace.telestion.api.message.JsonMessage;
+
+public record TransformedMessage(
+		@JsonProperty
+		String zeug
+) implements JsonMessage {
+}
