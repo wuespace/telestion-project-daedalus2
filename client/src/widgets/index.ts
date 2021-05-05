386a0d838b61c0b9f7a61af3630bef4e1ad6bdf9
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-05-05 18:02:59 +0200

    feat(widget): Initialize PGraph widget

--- client/src/widgets/index.ts
+++ client/src/widgets/index.ts
@@ -7,9 +7,11 @@ using the tc-cli generate widget command.
 
 import { Widget } from '@wuespace/telestion-client-types';
 import { widget as sampleWidget } from './sample-widget';
+import { widget as pgraphWidget } from './pgraph-widget';
 // IMPORT_INSERT_MARK
 
 export const projectWidgets: Widget[] = [
 	// ARRAY_FIRST_ELEMENT_INSERT_MARK
+	pgraphWidget,
 	sampleWidget
 ];
