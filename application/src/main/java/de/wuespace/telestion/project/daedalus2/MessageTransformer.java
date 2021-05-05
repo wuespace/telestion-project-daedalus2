2c0b8a75b3de696bc8577810a6c517cb1177e518
Pablo Klaschka <contact@pabloklaschka.de>
2021-05-05 18:51:24 +0200

    docs(message-transformer): Document and reformat the `de.wuespace.telestion.project.daedalus2.MessageTransformer` verticle

--- application/src/main/java/de/wuespace/telestion/project/daedalus2/MessageTransformer.java
+++ application/src/main/java/de/wuespace/telestion/project/daedalus2/MessageTransformer.java
@@ -8,6 +8,15 @@ import de.wuespace.telestion.project.daedalus2.messages.*;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 
+/**
+ * Listens for {@link Drehtest} messages and converts them to further "beautified" {@link SystemT} messages.<br />
+ * <p>
+ * These new messages, among other enhancements, add hierarchy to the data (making it easier to query specific
+ * sets of data) and transforms int-encoded bit sequences to objects.<br />
+ * <p>
+ * It gets configured using {@link Configuration} with {@link Configuration#inAddress} (which it listens to) and
+ * {@link Configuration#outAddress}, which it publishes to.
+ */
 public class MessageTransformer extends AbstractVerticle {
 	private Configuration config;
 
@@ -29,12 +38,40 @@ public class MessageTransformer extends AbstractVerticle {
 		eb.consumer(this.config.inAddress(), raw -> JsonMessage.on(Drehtest.class, raw, message -> {
 			var beautifulMessage = new SystemT(
 					message.timeLocal(),
-					new Imu(new Accelerometer(message.imuAccX(), message.imuAccY(), message.imuAccZ()), new Gyro(message.imuGyroX(), message.imuGyroY(), message.imuGyroZ())),
+					new Imu(
+							new Accelerometer(
+									message.imuAccX(),
+									message.imuAccY(),
+									message.imuAccZ()
+							),
+							new Gyro(
+									message.imuGyroX(),
+									message.imuGyroY(),
+									message.imuGyroZ()
+							)
+					),
 					message.tachoRotRate(),
-					new ServoAmps(message.servoAmps()[0], message.servoAmps()[1], message.servoAmps()[2], message.servoAmps()[3]),
-					new BatVolts(message.batVolts()[0], message.batVolts()[1]),
-					new Filter(message.filterVelVertical(), message.filterVelVerticalInd(), message.filterHeightGround(), message.filterRotorRotRate(), message.filterBodyRotRate()),
-					new Controller(message.controllerBladePitch(), message.controllerFinAngle()),
+					new ServoAmps(
+							message.servoAmps()[0],
+							message.servoAmps()[1],
+							message.servoAmps()[2],
+							message.servoAmps()[3]
+					),
+					new BatVolts(
+							message.batVolts()[0],
+							message.batVolts()[1]
+					),
+					new Filter(
+							message.filterVelVertical(),
+							message.filterVelVerticalInd(),
+							message.filterHeightGround(),
+							message.filterRotorRotRate(),
+							message.filterBodyRotRate()
+					),
+					new Controller(
+							message.controllerBladePitch(),
+							message.controllerFinAngle()
+					),
 					message.controllerId(),
 					new Status(
 							(message.availableStatus() & 0b00000001) > 0,
@@ -54,6 +91,12 @@ public class MessageTransformer extends AbstractVerticle {
 		startPromise.complete();
 	}
 
+	/**
+	 * Configuration for the {@link MessageTransformer} verticle.
+	 *
+	 * @param inAddress  the address the verticle listens to
+	 * @param outAddress the address the verticle publishes to
+	 */
 	public record Configuration(
 			@JsonProperty
 			String inAddress,
