4ebf9b909e9950876b11aada5585c5d99a48b474
Jan Tischhöfer <jan.tischhoefer@gmail.com>
2021-05-08 23:41:43 +0200

    feat(database): added working database implementation
    No final version. Still to be improved.

---
+++ application/src/main/java/de/wuespace/telestion/project/daedalus2/example/SystemTPublisher.java
@@ -0,0 +1,38 @@
+package de.wuespace.telestion.project.daedalus2.example;
+
+import de.wuespace.telestion.project.daedalus2.database.DataListener;
+import de.wuespace.telestion.project.daedalus2.database.PeriodicDataAggregator;
+import de.wuespace.telestion.project.daedalus2.messages.SystemT;
+import de.wuespace.telestion.project.daedalus2.database.MongoDatabaseService;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+import java.time.Duration;
+import java.util.List;
+
+public class SystemTPublisher extends AbstractVerticle {
+	public static void main(String[] args) {
+		Vertx vertx = Vertx.vertx();
+		vertx.deployVerticle(new MongoDatabaseService("daedalus2", "d2Pool"));
+		vertx.deployVerticle(new PeriodicDataAggregator(
+				"de.wuespace.telestion.project.daedalus2.messages.SystemT",
+				"imu.acc.x",
+				1,
+				"aggregated-imu.acc.x"
+		));
+		vertx.deployVerticle(new DataListener(List.of("SystemT#out")));
+		// vertx.deployVerticle(new MessageLogger());
+		vertx.deployVerticle(new SystemTPublisher());
+	}
+
+	@Override
+	public void start(Promise<Void> startPromise) throws Exception {
+		vertx.setPeriodic(Duration.ofMillis(200).toMillis(), id -> {
+			for (int i = 0; i <= 9; i++) {
+				vertx.eventBus().publish("SystemT#out", new SystemT().json());
+			}
+		});
+		startPromise.complete();
+	}
+}
