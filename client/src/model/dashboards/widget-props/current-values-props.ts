cc174e183eb1ef13548058a281ba85ee5d3d0681
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-05-15 15:09:40 +0200

    fix(client): Use new event bus addresses for the fake data in the current values configuration

--- client/src/model/dashboards/widget-props/current-values-props.ts
+++ client/src/model/dashboards/widget-props/current-values-props.ts
@@ -3,12 +3,12 @@ import { CurrentValuesWidgetProps } from '../../../widgets/current-values-widget
 export const fakeProps: CurrentValuesWidgetProps = {
 	connections: [
 		{
-			address: 'fake-channel1',
+			address: 'aggregated-imu.acc.x',
 			title: 'Acc X (m/s²)',
 			rps: 30
 		},
 		{
-			address: 'fake-channel2',
+			address: 'aggregated-imu.acc.y',
 			title: 'Acc Y (m/s²)',
 			precision: 4,
 			rps: 20,
@@ -16,7 +16,7 @@ export const fakeProps: CurrentValuesWidgetProps = {
 			maxValueWarn: 7
 		},
 		{
-			address: 'fake-channel3',
+			address: 'aggregated-imu.acc.z',
 			title: 'Acc Z (m/s²)',
 			outdatedTimeWarn: 5000,
 			outdatedTimeError: 20000,
@@ -24,14 +24,14 @@ export const fakeProps: CurrentValuesWidgetProps = {
 			rps: 10
 		},
 		{
-			address: 'fake-channel4',
-			title: 'Acc X (m/s²)',
+			address: 'no-channel',
+			title: 'Acc 0 (m/s²)',
 			rps: 30,
 			minValueError: 4,
 			maxValueError: 6
 		},
 		{
-			address: 'fake-channel2',
+			address: 'aggregated-imu.acc.y',
 			title: 'Acc Y (m/s²)',
 			precision: 1,
 			rps: 20,
@@ -41,24 +41,24 @@ export const fakeProps: CurrentValuesWidgetProps = {
 			maxValueError: 9
 		},
 		{
-			address: 'fake-channel3',
+			address: 'aggregated-imu.acc.z',
 			title: 'Acc Z (m/s²)',
 			precision: 6,
 			rps: 10
 		},
 		{
-			address: 'fake-channel4',
-			title: 'Acc X (m/s²)',
+			address: 'no-channel',
+			title: 'Acc 0 (m/s²)',
 			rps: 30
 		},
 		{
-			address: 'fake-channel2',
+			address: 'aggregated-imu.acc.y',
 			title: 'Acc Y (m/s²)',
 			precision: 1,
 			rps: 20
 		},
 		{
-			address: 'fake-channel3',
+			address: 'aggregated-imu.acc.z',
 			title: 'Acc Z (m/s²)',
 			precision: 6,
 			rps: 10
