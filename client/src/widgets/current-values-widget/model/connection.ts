f0dab65bdb5e25c71956b737f54e1a53208223d7
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-05-15 03:57:28 +0200

    feat(widget): Implement delay status and context based text color in current-values widget

--- client/src/widgets/current-values-widget/model/connection.ts
+++ client/src/widgets/current-values-widget/model/connection.ts
@@ -22,6 +22,42 @@ export interface Connection
 	precision?: number;
 
 	/**
+	 * Switch the status to warn, if the delay exceeds the specified time.
+	 * When set to `undefined`, no status switch will occur.
+	 */
+	outdatedTimeWarn?: number;
+
+	/**
+	 * Switch the status to error, if the delay exceeds the specified time.
+	 * When set to `undefined`, no status switch will occur.
+	 */
+	outdatedTimeError?: number;
+
+	/**
+	 * If the current value drops below this specified value, it will be painted in a "notice" variant.
+	 * When set to `undefined` no painting will occur.
+	 */
+	minValueWarn?: number;
+
+	/**
+	 * If the current value drops below this specified value, it will be painted in a "negative" variant.
+	 * When set to `undefined` no painting will occur.
+	 */
+	minValueError?: number;
+
+	/**
+	 * If the current value increases above this specified value, it will be painted in a "notice" variant.
+	 * When set to `undefined` no painting will occur.
+	 */
+	maxValueWarn?: number;
+
+	/**
+	 * If the current value increases above this specified value, it will be painted in a "negative" variant.
+	 * When set to `undefined` no painting will occur.
+	 */
+	maxValueError?: number;
+
+	/**
 	 * Renders per second.
 	 */
 	rps: number;
