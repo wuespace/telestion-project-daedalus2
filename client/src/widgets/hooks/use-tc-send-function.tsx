d6f7685e7ebd416c35345e83c5cedd28191319ae
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-06-23 12:29:42 +0200

    style(client): Fix styling issues in use-tc-send-function.tsx

--- client/src/widgets/hooks/use-tc-send-function.tsx
+++ client/src/widgets/hooks/use-tc-send-function.tsx
@@ -4,8 +4,8 @@ import { useCallback } from 'react';
 export function useTcSendFunction(channel: string) {
 	const eb = useEventBus(state => state.eventBus);
 
-	return useCallback(cmd => eb?.publish(channel, { content: cmd }), [
-		eb,
-		channel
-	]);
+	return useCallback(
+		cmd => eb?.publish(channel, { content: cmd }),
+		[eb, channel]
+	);
 }
