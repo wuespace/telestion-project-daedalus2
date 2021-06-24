ec397d4f503006a1846f5ddaf5d5efda21e6fb35
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-06-24 19:37:24 +0200

    feat(widget): Make state widget compatible with Telestion Client Library v0.16

--- client/src/widgets/state-widget/index.ts
+++ client/src/widgets/state-widget/index.ts
@@ -3,5 +3,7 @@ import { Widget as WidgetRenderer } from './widget';
 
 export const widget: Widget = {
 	name: 'stateWidget',
+	title: 'State Widget',
+	version: '0.1.0',
 	Widget: WidgetRenderer
 };
