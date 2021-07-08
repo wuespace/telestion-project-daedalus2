8cdc324dadc92b7e23fcbde41f9da8858d76c6e6
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-07-08 14:26:35 +0200

    chore(client): Fix register pre-commit hook
    Closes #134

--- client/scripts/register-precommit-hook.js
+++ client/scripts/register-precommit-hook.js
@@ -8,29 +8,53 @@ const exec = util.promisify(child_process.exec);
 async function run() {
 	try {
 		const gitRoot = (await exec('git rev-parse --show-toplevel')).stdout.trim();
-		let hooksFolder = path.join(gitRoot, '.git', 'hooks');
+		// path to the hooks folder in git
+		const hooksFolder = path.join(gitRoot, '.git', 'hooks');
+		// path to the pre-commit hook
+		const preCommitPath = path.join(hooksFolder, 'pre-commit');
 
+		// create hooks folder if it doesn't exists
 		if (!fs.existsSync(hooksFolder)) {
 			fs.mkdirSync(hooksFolder);
 		}
 
+		// check if someone already has registered a pre-commit hook
+		if (fs.existsSync(preCommitPath)) {
+			console.warn(
+				'WARN: Someone else has already registered a pre-commit hook. ' +
+					'Deleting it to continue...'
+			);
+			fs.unlinkSync(preCommitPath);
+		}
+
+		// relative path used in pre-commit hook
 		const relativePath = path
 			.relative(gitRoot, path.join(__dirname, '..'))
 			.split('\\')
 			.join('/');
+		const cdPath = relativePath || '.';
 
-		const preCommitPath = path.join(hooksFolder, 'pre-commit');
-		if (!fs.existsSync(preCommitPath)) {
-			fs.writeFileSync(
-				preCommitPath,
-				`#!/bin/sh\nnpx pretty-quick --staged --pattern "${
-					relativePath ? relativePath + '/' : ''
-				}**/*"`
-			);
-		}
+		fs.writeFileSync(
+			preCommitPath,
+			// shebang
+			'#!/bin/sh\n' +
+				// store current pwd
+				'PWD="$(pwd)"\n' +
+				// go to client part
+				`cd "${cdPath}"\n` +
+				// run styling
+				'npx pretty-quick --staged\n' +
+				// go back to current pwd
+				'cd "$PWD"'
+		);
+
+		// make pre-commit executable
+		fs.chmodSync(preCommitPath, '755');
+
+		console.log('SUCCESS: Pre-commit hook registered at:', preCommitPath);
 	} catch (e) {
-		// fail silently
-		console.log('fail silently :P');
+		console.error('ERROR: Unknown error:', e);
+		process.exit(1);
 	}
 }
 
