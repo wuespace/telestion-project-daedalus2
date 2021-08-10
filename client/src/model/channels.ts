04a74b0f48e81f9d6258b447b67cfe5c53a4fa78
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-08-10 03:36:22 +0200

    feat(client): Add the latest-state channel name and type to the project

--- client/src/model/channels.ts
+++ client/src/model/channels.ts
@@ -1,2 +1,7 @@
+import { MongoPick, SystemT } from './mongo-collections';
+
 export const telecommandClassName =
 	'de.wuespace.telestion.project.daedalus2.mavlink.internal.Telecommand';
+
+export type LatestState = MongoPick<SystemT, 'currentState'>;
+export const latestStateChannel = 'latest-state';
