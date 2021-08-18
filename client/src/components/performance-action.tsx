68ce7f3ef6cad435c1940827968e644d050e961d
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-08-18 13:40:56 +0200

    feat(client): Add performance action

---
+++ client/src/components/performance-action.tsx
@@ -0,0 +1,36 @@
+import { useCallback, useState } from 'react';
+import { TooltipTrigger, Tooltip, ActionButton } from '@adobe/react-spectrum';
+
+import Gauge1 from '@spectrum-icons/workflow/Gauge1';
+import Gauge2 from '@spectrum-icons/workflow/Gauge2';
+import Gauge3 from '@spectrum-icons/workflow/Gauge3';
+import Gauge4 from '@spectrum-icons/workflow/Gauge4';
+import Gauge5 from '@spectrum-icons/workflow/Gauge5';
+
+export function PerformanceAction() {
+	const [state, setState] = useState(1);
+
+	const handle = useCallback(
+		() => setState(prevState => (prevState < 5 ? prevState + 1 : 1)),
+		[]
+	);
+
+	return (
+		<TooltipTrigger>
+			<ActionButton onPress={handle}>
+				{state === 1 ? (
+					<Gauge1 />
+				) : state === 2 ? (
+					<Gauge2 />
+				) : state === 3 ? (
+					<Gauge3 />
+				) : state === 4 ? (
+					<Gauge4 />
+				) : (
+					<Gauge5 />
+				)}
+			</ActionButton>
+			<Tooltip>{state < 5 ? 'Increase performance' : 'Reset'}</Tooltip>
+		</TooltipTrigger>
+	);
+}
