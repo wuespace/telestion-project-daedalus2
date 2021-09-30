49374349e49981763a789a13bc1619ddf1cffb71
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-09-30 02:47:45 +0200

    feat(application): Add A-GPS transmitter verticle

---
+++ application/src/main/java/de/wuespace/telestion/project/daedalus2/gps/AGpsData.java
@@ -0,0 +1,10 @@
+package de.wuespace.telestion.project.daedalus2.gps;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import de.wuespace.telestion.api.message.JsonMessage;
+
+public record AGpsData(@JsonProperty String name, @JsonProperty String encodedData) implements JsonMessage {
+	public AGpsData() {
+		this(null, null);
+	}
+}
