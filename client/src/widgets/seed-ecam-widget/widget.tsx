3db883acab460c0177276eb85eb9f1bb5be0f97d
Pablo Klaschka <contact@pabloklaschka.de>
2021-10-12 14:00:45 +0200

    fix(client): Fixed a bug where the Seed ECAM widget would crash after a "once connected" event bus got disconnected, requiring a Widget reload (in the standard widget error screen) to reload the widget

--- client/src/widgets/seed-ecam-widget/widget.tsx
+++ client/src/widgets/seed-ecam-widget/widget.tsx
@@ -20,93 +20,109 @@ export function Widget({ seed, voltThreshold, ampsThreshold }: WidgetProps) {
 				{/*@ts-ignore*/}
 				<LoadingIndicator dependencies={[data]}>
 					{data => (
-						<SeedECAMSVG
-							{...valve(
-								'bat1Main',
-								data.bat1Allowed,
-								data.bat1 > voltThreshold
+						<>
+							{data && (
+								<SeedECAMSVG
+									{...valve(
+										'bat1Main',
+										data.bat1Allowed,
+										data.bat1 > voltThreshold
+									)}
+									{...activeInactive('bat1Input', data.bat1 > voltThreshold)}
+									{...activeInactive('bat1MainEffective', data.bat1Used)}
+									bat1Volts={data.bat1.toFixed(2) + ' V'}
+									{...valve(
+										'bat2Main',
+										data.bat2Allowed,
+										data.bat2 > voltThreshold
+									)}
+									{...activeInactive('bat2Input', data.bat2 > voltThreshold)}
+									{...activeInactive('bat2MainEffective', data.bat2Used)}
+									bat2Volts={data.bat2.toFixed(2) + ' V'}
+									{...valve(
+										'rxsmMain',
+										data.rxsmAllowed,
+										data.rxsmVolts > voltThreshold
+									)}
+									{...activeInactive(
+										'rxsmInput',
+										data.rxsmVolts > voltThreshold
+									)}
+									{...activeInactive('rxsmMainEffective', data.rxsmUsed)}
+									rxsmVolts={data.rxsmVolts.toFixed(2) + ' V'}
+									/* TODO: Add back in if suitable TM gets added
+									{...valve(
+										'heater',
+										true,
+										data.batHeatingAmps > ampsThreshold
+									)} */
+									heaterValveOffLine={''}
+									heaterValveBG={inactive}
+									{...activeInactive(
+										'heaterValveBorder',
+										data.rxsmVolts > voltThreshold
+									)}
+									{...activeInactive(
+										'heaterValveOnLine',
+										data.rxsmVolts > voltThreshold
+									)}
+									{...activeInactive(
+										'heaterBlock',
+										data.rxsmVolts > voltThreshold
+									)}
+									heaterAmps={'-NoData-'}
+									{...activeInactive(
+										'dcdcOutBlock',
+										data.rail3V3Volts > voltThreshold
+									)}
+									dcdcOut1Volts={data.rail3V3Volts.toFixed(2) + ' V'}
+									dcdcOut2Volts={data.rail5Volts.toFixed(2) + ' V'}
+									{...onOffSwitch(
+										'servoTrunk',
+										data.mainBusVoltage < 9.2,
+										data.mainBusVoltage > voltThreshold
+									)}
+									{...activeInactive(
+										'servoTrunk',
+										data.mainBusVoltage < 9.2 &&
+											data.mainBusVoltage > voltThreshold
+									)}
+									vccVolts={data.mainBusVoltage.toFixed(2) + ' V'}
+									{...activeInactive(
+										'vccBus',
+										data.mainBusVoltage > voltThreshold
+									)}
+									sFinnenAmps={
+										(data.fin_servo_amps * 1000).toPrecision(3) + ' mA'
+									}
+									st1Amps={
+										(data.swashplate_servo1_amps * 1000).toPrecision(3) + ' mA'
+									}
+									st2Amps={
+										(data.swashplate_servo2_amps * 1000).toPrecision(3) + ' mA'
+									}
+									st3Amps={
+										(data.swashplate_servo3_amps * 1000).toPrecision(3) + ' mA'
+									}
+									{...activeInactive(
+										'sFinnenBlock',
+										data.fin_servo_amps > ampsThreshold
+									)}
+									{...activeInactive(
+										'st1Block',
+										data.swashplate_servo1_amps > ampsThreshold
+									)}
+									{...activeInactive(
+										'st2Block',
+										data.swashplate_servo2_amps > ampsThreshold
+									)}
+									{...activeInactive(
+										'st3Block',
+										data.swashplate_servo3_amps > ampsThreshold
+									)}
+								/>
 							)}
-							{...activeInactive('bat1Input', data.bat1 > voltThreshold)}
-							{...activeInactive('bat1MainEffective', data.bat1Used)}
-							bat1Volts={data.bat1.toFixed(2) + ' V'}
-							{...valve(
-								'bat2Main',
-								data.bat2Allowed,
-								data.bat2 > voltThreshold
-							)}
-							{...activeInactive('bat2Input', data.bat2 > voltThreshold)}
-							{...activeInactive('bat2MainEffective', data.bat2Used)}
-							bat2Volts={data.bat2.toFixed(2) + ' V'}
-							{...valve(
-								'rxsmMain',
-								data.rxsmAllowed,
-								data.rxsmVolts > voltThreshold
-							)}
-							{...activeInactive('rxsmInput', data.rxsmVolts > voltThreshold)}
-							{...activeInactive('rxsmMainEffective', data.rxsmUsed)}
-							rxsmVolts={data.rxsmVolts.toFixed(2) + ' V'}
-							/* TODO: Add back in if suitable TM gets added
-							{...valve(
-								'heater',
-								true,
-								data.batHeatingAmps > ampsThreshold
-							)} */
-							heaterValveOffLine={''}
-							heaterValveBG={inactive}
-							{...activeInactive(
-								'heaterValveBorder',
-								data.rxsmVolts > voltThreshold
-							)}
-							{...activeInactive(
-								'heaterValveOnLine',
-								data.rxsmVolts > voltThreshold
-							)}
-							{...activeInactive('heaterBlock', data.rxsmVolts > voltThreshold)}
-							heaterAmps={'-NoData-'}
-							{...activeInactive(
-								'dcdcOutBlock',
-								data.rail3V3Volts > voltThreshold
-							)}
-							dcdcOut1Volts={data.rail3V3Volts.toFixed(2) + ' V'}
-							dcdcOut2Volts={data.rail5Volts.toFixed(2) + ' V'}
-							{...onOffSwitch(
-								'servoTrunk',
-								data.mainBusVoltage < 9.2,
-								data.mainBusVoltage > voltThreshold
-							)}
-							{...activeInactive(
-								'servoTrunk',
-								data.mainBusVoltage < 9.2 && data.mainBusVoltage > voltThreshold
-							)}
-							vccVolts={data.mainBusVoltage.toFixed(2) + ' V'}
-							{...activeInactive('vccBus', data.mainBusVoltage > voltThreshold)}
-							sFinnenAmps={(data.fin_servo_amps * 1000).toPrecision(3) + ' mA'}
-							st1Amps={
-								(data.swashplate_servo1_amps * 1000).toPrecision(3) + ' mA'
-							}
-							st2Amps={
-								(data.swashplate_servo2_amps * 1000).toPrecision(3) + ' mA'
-							}
-							st3Amps={
-								(data.swashplate_servo3_amps * 1000).toPrecision(3) + ' mA'
-							}
-							{...activeInactive(
-								'sFinnenBlock',
-								data.fin_servo_amps > ampsThreshold
-							)}
-							{...activeInactive(
-								'st1Block',
-								data.swashplate_servo1_amps > ampsThreshold
-							)}
-							{...activeInactive(
-								'st2Block',
-								data.swashplate_servo2_amps > ampsThreshold
-							)}
-							{...activeInactive(
-								'st3Block',
-								data.swashplate_servo3_amps > ampsThreshold
-							)}
-						/>
+						</>
 					)}
 				</LoadingIndicator>
 			</View>
