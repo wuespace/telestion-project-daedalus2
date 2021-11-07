9a954523019129c79f528e06d8c4423699688d03
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-11-07 16:43:46 +0100

    feat(application): Add message mapper verticle to map iridium messages to redis readable data

---
+++ application/src/main/java/de/wuespace/telestion/project/daedalus2/iridium/MappedMessage.java
@@ -0,0 +1,16 @@
+package de.wuespace.telestion.project.daedalus2.iridium;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import de.wuespace.telestion.api.message.JsonMessage;
+import de.wuespace.telestion.project.daedalus2.iridium.message.IEHeader;
+import de.wuespace.telestion.project.daedalus2.iridium.message.IELocation;
+import de.wuespace.telestion.project.daedalus2.iridium.message.IEPayload;
+
+public record MappedMessage(
+		@JsonProperty IEHeader header,
+		@JsonProperty IEPayload payload,
+		@JsonProperty IELocation location) implements JsonMessage {
+	public MappedMessage() {
+		this(null, null, null);
+	}
+}
