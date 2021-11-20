c2c9f6102f8faf2b29a741825fd36128fbb52fd6
Pablo Klaschka <contact@pabloklaschka.de>
2021-11-20 12:50:45 +0100

    fix(client): Fix several small visual issues in the Seed ECAM Widget

--- client/src/widgets/seed-ecam-widget/hooks/use-seed-ecam-data.tsx
+++ client/src/widgets/seed-ecam-widget/hooks/use-seed-ecam-data.tsx
@@ -1,27 +1,27 @@
 import { useCachedLatest } from '../../hooks';
 
 export function useSeedEcamData(seed: 'seedA' | 'seedB') {
-	const [systemT] = useCachedLatest<Record<string, any>[]>([
+	const [seedHeartbeat] = useCachedLatest<Record<string, any>[]>([
 		`latest/${seed}/SEED_HEARTBEAT`
 	]);
 
-	const data = systemT && {
+	const data = seedHeartbeat && {
 		// batHeatingAmps: Number.NaN,
-		fin_servo_amps: systemT.adc_measurements_cop[0] / 1000,
-		swashplate_servo1_amps: systemT.adc_measurements_cop[1] / 1000,
-		swashplate_servo2_amps: systemT.adc_measurements_cop[2] / 1000,
-		swashplate_servo3_amps: systemT.adc_measurements_cop[3] / 1000,
-		rxsmAllowed: (systemT?.bat_status & 0b001) > 0,
-		rxsmUsed: (systemT?.bat_status & 0b1000) > 0,
-		bat1Allowed: (systemT?.bat_status & 0b010) > 0,
-		bat1Used: (systemT?.bat_status & 0b010000) > 0,
-		bat2Allowed: (systemT?.bat_status & 0b100) > 0,
-		bat2Used: (systemT?.bat_status & 0b100000) > 0,
-		bat1: systemT?.adc_measurements_cop[7] / 1000,
-		bat2: systemT?.adc_measurements_cop[6] / 1000,
-		rxsmVolts: systemT?.adc_measurements_sbc[0] / 1000,
-		rail3V3Volts: systemT.adc_measurements_sbc[1] / 1000,
-		rail5Volts: systemT.adc_measurements_sbc[7] / 1000
+		fin_servo_amps: seedHeartbeat.adc_measurements_cop[0] / 1000,
+		swashplate_servo1_amps: seedHeartbeat.adc_measurements_cop[1] / 1000,
+		swashplate_servo2_amps: seedHeartbeat.adc_measurements_cop[2] / 1000,
+		swashplate_servo3_amps: seedHeartbeat.adc_measurements_cop[3] / 1000,
+		rxsmAllowed: (seedHeartbeat?.bat_status & 0b001) > 0,
+		rxsmUsed: (seedHeartbeat?.bat_status & 0b1000) > 0,
+		bat1Allowed: (seedHeartbeat?.bat_status & 0b010) > 0,
+		bat1Used: (seedHeartbeat?.bat_status & 0b010000) > 0,
+		bat2Allowed: (seedHeartbeat?.bat_status & 0b100) > 0,
+		bat2Used: (seedHeartbeat?.bat_status & 0b100000) > 0,
+		bat1: seedHeartbeat?.adc_measurements_cop[7] / 1000,
+		bat2: seedHeartbeat?.adc_measurements_cop[6] / 1000,
+		rxsmVolts: seedHeartbeat?.adc_measurements_sbc[0] / 1000,
+		rail3V3Volts: seedHeartbeat.adc_measurements_sbc[1] / 1000,
+		rail5Volts: seedHeartbeat.adc_measurements_sbc[7] / 1000
 	};
 
 	// TODO: Incorporate bat_used into mainBusVoltage calculation
