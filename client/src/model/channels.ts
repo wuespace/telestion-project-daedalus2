35e446a1fae3abec2d20cd0c9157c6092158e1c3
Ludwig Richter <ludwig.richter@wuespace.de>
2022-01-12 16:34:09 +0100

    fix(client): Fix Telecommand class name due to previous refactor (see v0.6.2 integration)

--- client/src/model/channels.ts
+++ client/src/model/channels.ts
@@ -1,7 +1,7 @@
 import { MongoPick, SystemT } from './mongo-collections';
 
 export const telecommandClassName =
-	'de.wuespace.telestion.project.daedalus2.mavlink.internal.Telecommand';
+	'de.wuespace.telestion.project.daedalus2.mavlink.message.Telecommand';
 
 export type LatestState = MongoPick<SystemT, 'currentState'>;
 export const latestStateChannel = 'latest-state';
