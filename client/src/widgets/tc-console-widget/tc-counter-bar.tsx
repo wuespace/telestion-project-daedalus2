3ef7177fe8208cc6fa5ce763a2bd66ec132e4ea2
Ludwig Richter <ludwig.richter@wuespace.de>
2022-01-12 16:35:18 +0100

    feat(widget): Finalize TC console widget

---
+++ client/src/widgets/tc-console-widget/tc-counter-bar.tsx
@@ -0,0 +1,43 @@
+import { useMemo } from 'react';
+import { Divider, Flex, Text, View } from '@adobe/react-spectrum';
+import { useCachedLatest } from '../hooks';
+import { ConsoleDefinition } from './model';
+
+const seedTcCounterPostfix = 'SEED_HEARTBEAT/telecommand_cnt';
+const ejectorTcCounterPostfix = 'EJECTOR_HEARTBEAT/telecommand_cnt';
+
+export interface TcCounterProps {
+	definitions: ConsoleDefinition[];
+}
+
+export function TcCounterBar({ definitions }: TcCounterProps) {
+	const keys = useMemo(
+		() =>
+			definitions.map(definition =>
+				definition.name === 'ejector'
+					? `latest/${definition.name}/${ejectorTcCounterPostfix}`
+					: `latest/${definition.name}/${seedTcCounterPostfix}`
+			),
+		[definitions]
+	);
+
+	const values = useCachedLatest<number[]>(keys);
+
+	return (
+		<View paddingY="size-100">
+			<Flex direction="row" gap="size-100">
+				{definitions.map((definition, index) => (
+					<>
+						<Text>
+							{definition.title}: {values[index] || 'N/A'}
+						</Text>
+						{/* add divider for all but last entry */}
+						{index < definitions.length - 1 && (
+							<Divider orientation="vertical" size="S" />
+						)}
+					</>
+				))}
+			</Flex>
+		</View>
+	);
+}
---
+++ client/src/widgets/tc-console-widget/telecommand-bar.tsx
@@ -0,0 +1,62 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+import { Flex, Item, Picker, TextField, View } from '@adobe/react-spectrum';
+import { TCSendButton } from '../components/tc-send-button';
+import { ConsoleDefinition } from './model';
+import { TCState } from '../../model/tc-state';
+
+export interface TelecommandBarProps {
+	definitions: ConsoleDefinition[];
+	target: string;
+	tcState: TCState;
+	setTarget: Dispatch<SetStateAction<string>>;
+	setTcState: Dispatch<SetStateAction<TCState>>;
+}
+
+export function TelecommandBar({
+	definitions,
+	target,
+	tcState,
+	setTarget,
+	setTcState
+}: TelecommandBarProps) {
+	const [telecommand, setTelecommand] = useState('');
+
+	return (
+		<View>
+			<Flex
+				direction="row"
+				gap="size-100"
+				justifyContent="space-between"
+				alignItems="end"
+			>
+				<Picker
+					label="Target (synced to tab)"
+					items={definitions}
+					selectedKey={target}
+					isDisabled={tcState === TCState.SENDING}
+					// @ts-ignore
+					onSelectionChange={setTarget}
+				>
+					{item => <Item key={item.name}>{item.title}</Item>}
+				</Picker>
+				<TextField
+					label="Send command"
+					placeholder="test_led 1"
+					isRequired
+					isDisabled={tcState === TCState.SENDING}
+					value={telecommand}
+					onChange={setTelecommand}
+					flexGrow={1}
+				/>
+				<TCSendButton
+					onStateChange={setTcState}
+					command={telecommand}
+					target={target as any}
+					variant="cta"
+				>
+					Send TC&hellip;
+				</TCSendButton>
+			</Flex>
+		</View>
+	);
+}
