02883dff59bc8ee0cd986b2f99ba4a3d00fb5265
Ludwig Richter <ludwig.richter@wuespace.de>
2022-01-12 09:50:55 +0100

    feat(client): Add TC console widget

--- client/src/model/dashboards/debug-dashboard.ts
+++ client/src/model/dashboards/debug-dashboard.ts
@@ -2,12 +2,18 @@ import { Dashboard } from '@wuespace/telestion-client-types';
 
 export const debugDashboard: Dashboard = {
 	title: 'Debug',
-	columns: 1,
+	columns: 2,
 	rows: 1,
 	widgets: [
 		{
 			id: 'debug-0',
 			widgetName: 'eventbusDebugWidget',
+			width: 1,
+			height: 1
+		},
+		{
+			id: 'debug-1',
+			widgetName: 'tcCounterWidget',
 			width: 1,
 			height: 1
 		}
