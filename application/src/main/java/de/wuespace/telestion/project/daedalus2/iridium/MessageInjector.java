85a14441a942fb7e591d65521f835b753b413d53
Ludwig Richter <ludwig.richter@wuespace.de>
2023-02-10 14:08:14 +0100

    feat(application): Update Iridium message injector to send both latest and valid positions

--- application/src/main/java/de/wuespace/telestion/project/daedalus2/iridium/MessageInjector.java
+++ application/src/main/java/de/wuespace/telestion/project/daedalus2/iridium/MessageInjector.java
@@ -36,20 +36,20 @@ public class MessageInjector extends TelestionVerticle<MessageInjector.Configura
 	private void sendSeedAMessage(Instant sentTime) {
 		var header = new IEHeader(0, getConfig().seedAImei(), 0, 1, 1, sentTime.getEpochSecond());
 		var location = new IELocation(-1, -1, 67.887, 21.0851, 10);
-		var payload = new IEPayload(new ValidDaedalus2Payload(true, 32, false, 49.9, 9.9, 0));
+		var latestPayload = new IEPayload(new ValidDaedalus2Payload(true, 32, false, 67.887, 21.0851, 0));
+		var validPayload = new IEPayload(new ValidDaedalus2Payload(true, 32, true, 67.943, 20.9776, 0));
 
-		var iridiumMessage = new IridiumMessage(List.of(header, location, payload));
-
-		publish(getConfig().outAddress(), iridiumMessage);
+		publish(getConfig().outAddress(), new IridiumMessage(List.of(header, location, latestPayload)));
+		publish(getConfig().outAddress(), new IridiumMessage(List.of(header, location, validPayload)));
 	}
 
 	private void sendSeedBMessage(Instant sentTime) {
 		var header = new IEHeader(0, getConfig().seedBImei(), 0, 1, 1, sentTime.getEpochSecond());
 		var location = new IELocation(-1, -1, 67.921, 21.5422, 10);
-		var payload = new IEPayload(new ValidDaedalus2Payload(true, 32, false, 49.9, 9.9, 0));
+		var latestPayload = new IEPayload(new ValidDaedalus2Payload(true, 32, false, 67.921, 21.5422, 0));
+		var validPayload = new IEPayload(new ValidDaedalus2Payload(true, 32, true, 68.123, 21.3422, 0));
 
-		var iridiumMessage = new IridiumMessage(List.of(header, location, payload));
-
-		publish(getConfig().outAddress(), iridiumMessage);
+		publish(getConfig().outAddress(), new IridiumMessage(List.of(header, location, latestPayload)));
+		publish(getConfig().outAddress(), new IridiumMessage(List.of(header, location, validPayload)));
 	}
 }
