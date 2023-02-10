a0bd4e4fb9b2c58cf5b119b71a2325971a614a87
Ludwig Richter <ludwig.richter@wuespace.de>
2023-02-10 14:04:44 +0100

    feat(client/widget): Add Iridium map widget

--- client/src/widgets/iridium-map-widget/index.ts
+++ client/src/widgets/iridium-map-widget/index.ts
@@ -2,8 +2,8 @@ import { Widget } from '@wuespace/telestion-client-types';
 import { Widget as WidgetRenderer } from './widget';
 
 export const widget: Widget = {
-	name: 'mapWidget',
-	title: 'map-widget',
+	name: 'iridiumMapWidget',
+	title: 'iridium-map-widget',
 	version: '0.0.0',
 	Widget: WidgetRenderer
 };
